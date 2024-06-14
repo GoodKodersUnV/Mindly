e0515fc1699bbe5d05ede9357f238b928017d56f
Kalyan Tingani <kalyantingani@gmail.com>
2024-06-14 15:58:28 +0530

    path

--- src/app/(dashboard)/learn/[learnId]/page.tsx
+++ src/app/(dashboard)/learn/[learnId]/page.tsx
@@ -1,26 +1,43 @@
-import { getModulesbyQuizId } from '@/actions/quizzes'
-import Link from 'next/link'
-import React from 'react'
+import { getModulesbyQuizId } from "@/actions/quizzes";
+import Link from "next/link";
+import React from "react";
 
-const page = async ({ params }: {
-  params: { learnId: string }
-}) => {
+const page = async ({ params }: { params: { learnId: string } }) => {
   const modules = await getModulesbyQuizId(params.learnId);
   return (
-    <div>
+    <div className="w-1/2">
       {modules.map((module) => (
         <div key={module.id}>
           <div>
-            {module.title}
-            {module.submodules.map((submodule) => (
-              <div key={submodule.id}>
-                <Link href={`${params.learnId}/level/${submodule.id}`} >{submodule.level}</Link>
+            <div className="rounded-xl border flex justify-between items-center p-4">
+              <div>
+                <h1 className="text-2xl font-semibold">{module.title}</h1>
+                <h1>Description</h1>
               </div>
-            ))}
+              <div>
+                <button className="p-2 px-4 rounded-lg border">Continue</button>
+              </div>
+            </div>
+            <div className="my-10  ">
+              {module.submodules.map((submodule, index) => {
+                  // it should be curved like a game candy crush or something roadmap like curved 
+                const pad = index % 2 === 0 ? "8" : "2";
+                return (
+                  <div key={submodule.id} className={`my-6 p-${pad} mx-[300px] `}>
+                    <Link
+                      className="rounded-full border p-2"
+                      href={`${params.learnId}/level/${submodule.id}`}
+                    >
+                      {submodule.level}
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       ))}
     </div>
-  )
-}
-export default page
+  );
+};
+export default page;
