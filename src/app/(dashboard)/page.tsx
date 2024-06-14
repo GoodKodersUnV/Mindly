c820dda28c2c8f2b978205d9f896c99d9324b131
Kalyan Tingani <kalyantingani@gmail.com>
2024-06-15 00:05:58 +0530

    dashboard

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,38 +1,19 @@
 import getCurrentUser from "@/actions/getCurrentUser";
+import Image from "next/image";
 import React from "react";
-import { MdOutlineNoteAlt } from "react-icons/md";
-import { SiGoogleclassroom } from "react-icons/si";
+import { IoGameController } from "react-icons/io5";
 
-const page = async() => {
+const page = async () => {
   const currentUser = await getCurrentUser();
   return (
-    <div className="h-60 bg-gradient-to-l from-blue-400 to-blue-600 m-2 rounded-lg">
+    <div className="h-60 bg-gradient-to-r from-blue-500 to-purple-400 m-2 rounded-lg flex justify-between items">
       <div className="p-10">
         <h1 className="text-secondary-50 font-bold text-2xl">
-          Welcome back {currentUser?.name} 👋
+          Back to play {currentUser?.name} 👋
         </h1>
-        <p className="text-secondary-50 font-medium text-base mt-3">
-          Here is your report
-        </p>
       </div>
-      <div className="flex mb-10 p-2 text-center gap-4 justify-center flex-wrap">
-        <div className="w-80 rounded-md shadow-xl p-2 bg-secondary-50 text-secondary-900">
-          <MdOutlineNoteAlt className="m-auto h-24 w-24 text-blue-400" />
-          <p className="text-primary-600 font-bold pt-2">5</p>
-          <h1 className="p-1 text-sm font-bold">No of courses created</h1>
-        </div>
-        <div className="w-80 rounded-md shadow-xl bg-secondary-50 text-secondary-900 p-2">
-          <SiGoogleclassroom className="m-auto h-24 w-24 text-blue-400" />
-          <p className="text-primary-600 font-bold pt-2">6</p>
-          <h1 className="p-1 text-sm font-bold">
-            Total no of classes uploaded
-          </h1>
-        </div>
-        <div className="w-80 rounded-md shadow-xl bg-secondary-50 text-secondary-900 p-2">
-          {/* <PiStudentBold className="m-auto h-24 w-24 text-blue-400" /> */}icon
-          <p className="text-primary-600 font-bold pt-2">5</p>
-          <h1 className="p-1 text-sm font-bold">Total no of students</h1>
-        </div>
+      <div className="pe-40">
+        <IoGameController className="-rotate-12 text-[150px] mt-12 text-violet-500/60" />
       </div>
     </div>
   );
